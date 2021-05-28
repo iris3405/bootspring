8644c1a75d124000c0cc965db68feecb86576433
iris3405 <78770213+iris3405@users.noreply.github.com>
2021-05-28 17:36:03 +0900

    Add files via upload

---
+++ home/src/main/java/com/austin/home/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.austin.home.repository;
+
+import com.austin.home.model.Board;
+
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
---
+++ home/src/main/java/com/austin/home/repository/JpaRepository.java
@@ -0,0 +1,4 @@
+package com.austin.home.repository;
+
+public interface JpaRepository<T, T1> {
+}
